30ed544436c75465e9557636afd18e007fd57bbb
Daiki Shiozawa <me@nacl30d.com>
2023-05-15 00:34:47 +0900

    :sparkles: Create Header

--- apps/web/src/consts.ts
+++ apps/web/src/consts.ts
@@ -1,5 +1,13 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+import type { MenuItem } from './components/Header.astro';
 
 export const SITE_TITLE = 'My personal website.';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const MENU_ITEMS = [
+    { text: 'お知らせ', link: '#information' },
+    { text: 'コンサート', link: '#concert' },
+    { text: '私たちについて', link: '#about-us' },
+    { text: 'お問い合わせ', link: '#contact' },
+] satisfies MenuItem[];
