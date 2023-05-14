3d5f45a69441b061358ddfe7d9105f47af6acb02
Daiki Shiozawa <me@nacl30d.com>
2023-05-14 22:48:59 +0900

    :gem: Configure font-weight for Noto Sans JP

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -4,11 +4,22 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {},
-	plugins: [],
-	extend: {
-		fontFamily: {
-			sans: ['Noto Sans JP', ...defaultTheme.fontFamily.sans],
+	theme: {
+		fontWeight: {
+			// NOTE: Optimize for Noto Sans JP
+			thin: '100',
+			light: '300',
+			normal: '400',
+			medium: '500',
+			bold: '700',
+			extrabold: '900',
+		},
+		extend: {
+			fontFamily: {
+				sans: ['Noto Sans JP', ...defaultTheme.fontFamily.sans],
+			},
 		},
 	},
+	plugins: [],
+	extend: {},
 }
