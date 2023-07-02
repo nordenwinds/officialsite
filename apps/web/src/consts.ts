a27d3ca49fb9cb282c8f83042ba215749e6eb26b
Daiki Shiozawa <me@nacl30d.com>
2023-07-03 01:46:07 +0900

    :sparkles: Write contents

--- apps/web/src/consts.ts
+++ apps/web/src/consts.ts
@@ -1,13 +1,24 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-import type { MenuItem } from './components/Header.astro';
+export const SITE_TITLE = 'Norden Wind Ensemble';
+export const SITE_DESCRIPTION = 'Norden Wind Ensemble公式サイト';
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const NWE_DESCRIPTIONS = [
+    'Norden Wind Ensemble は北陵ウインドアンサンブルを前身とする、神奈川県茅ヶ崎市を中心に活動する吹奏楽団です。',
+    `北陵ウインドアンサンブルは神奈川県立茅ケ崎北陵高等学校吹奏楽部の卒業生で構成される吹奏楽団でした。
+2018年より活動方針を改め、団員を卒業生に限らない一般吹奏楽団として歩み始めました。`,
+    'Nordenは「北」を意味するドイツ語で、ルーツである北陵を表しています。',
+];
+
+export const CONTACT_FORM_URL = 'https://airtable.com/shrAMC1f9YZ9BPEpc';
+export const CONTACT_DESCRIPTIONS = [
+    'Norden Wind Ensembleへのお問い合わせは、お問い合わせフォームよりお願いいたします。',
+    '演奏会に関すること、出演依頼、相談など、お気軽にお問い合わせください。',
+];
 
 export const MENU_ITEMS = [
-    { text: 'お知らせ', link: '#information' },
-    { text: 'コンサート', link: '#concert' },
-    { text: '私たちについて', link: '#about-us' },
-    { text: 'お問い合わせ', link: '#contact' },
-] satisfies MenuItem[];
+    { text: 'お知らせ', link: '/#information' },
+    { text: 'コンサート', link: '/#concert' },
+    { text: '私たちについて', link: '/#about-us' },
+    { text: 'お問い合わせ', link: '/#contact' },
+];
