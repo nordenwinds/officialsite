cc608c012a7a3a5af43dd8d2cb9989e1d624c080
Daiki Shiozawa <me@nacl30d.com>
2023-07-16 01:19:48 +0900

    :sparkles: Update migration script

--- apps/cms/src/index.ts
+++ apps/cms/src/index.ts
@@ -1,18 +1,46 @@
-import dotenv from 'dotenv';
+import { strict as assert } from 'assert';
 import { runMigration } from 'contentful-migration';
-import SampleMigration from './migrations/16-typescript-migration.js';
+import { contentfulClient, createEnvironment } from './lib/contentful';
+import InitModels from './migrations/1-init_content_models.js';
+import { Environment, Space } from 'contentful-management';
 
-dotenv.config({ path: 'src/.env' });
+const migrate = async (space: Space, environment: Environment) => {
+    const options = {
+        environmentId: environment.sys.id,
+        spaceId: space.sys.id,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        yes: true,
+    };
 
-const options = {
-    environmentId: process.env.CONTENTFUL_ENV,
-    spaceId: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-    yes: true,
+    await runMigration({ ...options, ...{ migrationFunction: InitModels } });
 };
 
-const migrations = async () => {
-    await runMigration({ ...options, ...{ migrationFunction: SampleMigration } });
+const switchAlias = async (space: Space, environment: Environment, contentfulEnv: string) => {
+    const alias = await space.getEnvironmentAlias(contentfulEnv);
+    const currentEnvironmentId = alias.environment.sys.id;
+
+    console.log(`Switch ${contentfulEnv}: ${currentEnvironmentId} to ${environment.sys.id}`);
+    alias.environment.sys.id = environment.sys.id;
+    await alias.update();
+
+    console.log('Delete old environment', currentEnvironmentId);
+    (await space.getEnvironment(currentEnvironmentId)).delete();
 };
 
-migrations();
+const main = async () => {
+    const contentfulEnv = process.env.CONTENTFUL_ENV;
+    assert(contentfulEnv === 'develop' || contentfulEnv === 'production');
+    const spaceId = process.env.CONTENTFUL_SPACE_ID;
+    assert(spaceId);
+
+    const space = await contentfulClient.getSpace(spaceId);
+    const environment = await createEnvironment(space, contentfulEnv);
+
+    await migrate(space, environment);
+
+    if (contentfulEnv === 'production') {
+        await switchAlias(space, environment, contentfulEnv);
+    }
+};
+
+main();
---
+++ apps/cms/src/lib/contentful.ts
@@ -0,0 +1,63 @@
+import { strict as assert } from 'assert';
+import contentful, { Environment, Space } from 'contentful-management';
+
+const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
+assert(accessToken);
+
+export const contentfulClient = contentful.createClient({
+    accessToken: accessToken,
+});
+
+export const createEnvironment = async (space: Space, contentfulEnv: 'production' | 'develop'): Promise<Environment> => {
+    console.log(`Running on ${contentfulEnv}.`);
+
+    const environmentId = contentfulEnv === 'production' ? `${contentfulEnv}-${new Date().getTime()}` : contentfulEnv;
+    console.log('Environment ID', environmentId);
+
+    if (contentfulEnv !== 'production') {
+        console.log('Delete current environment.', environmentId);
+        await (await space.getEnvironment(environmentId)).delete();
+    }
+
+    const environment = await space.createEnvironmentWithId(environmentId, {
+        name: contentfulEnv,
+    });
+
+    console.log('Waiting for environment is ready...');
+    const maxNumberOfTries = 10;
+    const interval = 3000;
+    for (let tries = 0; tries < maxNumberOfTries; tries++) {
+        const status = (await space.getEnvironment(environment.sys.id)).sys.status.sys.id;
+
+        if (status === 'ready') {
+            console.log('Successfully created new environment.', environmentId);
+            break;
+        } else if (status === 'failed') {
+            console.error('Environment creation failed.');
+            process.exit(1);
+        }
+
+        await new Promise((resolve) => setTimeout(resolve, interval));
+    }
+
+    console.log('Update API keys to allow access to new environment.');
+    const newEnv = {
+        sys: {
+            type: 'Link',
+            linkType: 'Environment',
+            id: environment.sys.id,
+        },
+    };
+    const { items: keys } = await space.getApiKeys();
+    await Promise.all(
+        keys
+            .filter((key) => key.name === contentfulEnv)
+            .map((key) => {
+                console.log('Updating api key', key.sys.id);
+                key.environments.push(newEnv);
+                return key.update();
+            })
+    );
+
+    return environment;
+};
