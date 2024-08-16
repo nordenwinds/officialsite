d11e14145bcfcf5527fac74c178886b562437176
Daiki Shiozawa <me@nacl30d.com>
2024-08-16 23:46:40 +0900

    refactor: Update health check response

--- apps/functions/src/health/index.spec.ts
+++ apps/functions/src/health/index.spec.ts
@@ -3,13 +3,17 @@ import { testClient } from 'hono/testing';
 import { HttpStatus } from '../lib/http';
 import app from './index';
 import type { HealthCheckApp } from './index';
+import { HealthStatus } from './status';
 
 describe('health', () => {
   describe('get /', () => {
     it('should return ok', async () => {
       const res = await testClient<HealthCheckApp>(app).index.$get();
       expect(res.status).toBe(HttpStatus.OK);
-      expect(await res.json()).toEqual({ message: 'ok' });
+      expect(await res.json()).toEqual({
+        status: HealthStatus.PASS,
+        version: expect.any(String),
+      });
     });
   });
 });
--- apps/functions/src/health/index.ts
+++ apps/functions/src/health/index.ts
@@ -1,12 +1,15 @@
 import { Hono } from 'hono';
+import pkg from '../../package.json';
 import { HttpStatus } from '../lib/http';
+import { HealthStatus } from './status';
 
 const app = new Hono();
 
 const route = app.get('/', (c) => {
   return c.json(
     {
-      message: 'ok',
+      status: HealthStatus.PASS,
+      version: pkg.version,
     },
     HttpStatus.OK,
   );
