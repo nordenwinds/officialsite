26b66d217ab4af194003234777416adb7d56fc60
Daiki Shiozawa <me@nacl30d.com>
2025-01-25 16:29:41 +0900

    feat(web): Use type safe environment variables

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,5 +1,5 @@
 import mdx from '@astrojs/mdx';
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
@@ -9,4 +9,18 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
     site: 'https://www.nordenwinds.com',
     integrations: [mdx(), sitemap(), tailwind()],
+    env: {
+        schema: {
+            CONTENTFUL_ENV: envField.enum({
+                context: 'server',
+                access: 'public',
+                values: ['develop', 'master'],
+                default: 'develop',
+            }),
+            CONTENTFUL_SPACE_ID: envField.string({ context: 'server', access: 'public' }),
+            CONTENTFUL_DELIVERY_TOKEN: envField.string({ context: 'server', access: 'secret' }),
+            CONTENTFUL_PREVIEW_TOKEN: envField.string({ context: 'server', access: 'secret' }),
+            API_BASE_URL: envField.string({ context: 'client', access: 'public' }),
+        },
+    },
 });
--- apps/web/src/lib/contentful.ts
+++ apps/web/src/lib/contentful.ts
@@ -1,8 +1,9 @@
+import { CONTENTFUL_DELIVERY_TOKEN, CONTENTFUL_ENV, CONTENTFUL_PREVIEW_TOKEN, CONTENTFUL_SPACE_ID } from 'astro:env/server';
 import * as contentful from 'contentful';
 
 export const contentfulClient = contentful.createClient({
-    space: import.meta.env.CONTENTFUL_SPACE_ID,
-    accessToken: import.meta.env.DEV ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+    space: CONTENTFUL_SPACE_ID,
+    accessToken: import.meta.env.DEV ? CONTENTFUL_PREVIEW_TOKEN : CONTENTFUL_DELIVERY_TOKEN,
     host: import.meta.env.DEV ? 'preview.contentful.com' : 'cdn.contentful.com',
-    environment: import.meta.env.CONTENTFUL_ENV || 'master',
+    environment: CONTENTFUL_ENV,
 });
