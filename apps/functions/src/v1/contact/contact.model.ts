40a13a48db3d52bd7254b778611770a4a4274da6
Daiki Shiozawa <me@nacl30d.com>
2025-07-26 19:01:12 +0900

    chore: Follow update of middleware

--- apps/functions/src/v1/contact/contact.model.ts
+++ apps/functions/src/v1/contact/contact.model.ts
@@ -3,28 +3,27 @@ import { z } from '@hono/zod-openapi';
 export const ContactSchema = z
   .object({
     id: z
-      .string({ description: 'ID' })
+      .string()
       .uuid()
       .optional() // FIXME: .readonly()
       .openapi({
+        description: 'ID',
         example: '123',
       }),
-    name: z.string({ description: 'Name whom create a contact' }).openapi({
+    name: z.string().openapi({
+      description: 'Name whom create a contact',
       example: 'John Doe',
     }),
-    email: z
-      .string({ description: 'Email address whom create a contact' })
-      .email()
-      .openapi({
-        example: 'john@example.com',
-      }),
-    organization: z
-      .string({ description: 'Belongs whom create a contact' })
-      .optional()
-      .openapi({
-        example: 'Example Association',
-      }),
-    content: z.string({ description: 'Contact text' }).openapi({
+    email: z.string().email().openapi({
+      description: 'Email address whom create a contact',
+      example: 'john@example.com',
+    }),
+    organization: z.string().optional().openapi({
+      description: 'Belongs whom create a contact',
+      example: 'Example Association',
+    }),
+    content: z.string().openapi({
+      description: 'Contact text',
       example:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     }),
